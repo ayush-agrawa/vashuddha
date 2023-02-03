a365310b7b09583a3d5aa02f8d4c92502dba21ff
Mobcoder ID-134 <ayush.agarwal@getwise.in>
2023-02-03 18:28:47 +0530

    chat server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import './App.css';
-import Header from './component/Header';
-import Form from './component/Form';
-import Tabs from './component/Tabs';
-import LabTabs from './component/TabswithDefaultTabpanel';
 import Chat from './component/Chat';
 function App() {
   // fetch('https://dummyjson.com/products/1').then(
